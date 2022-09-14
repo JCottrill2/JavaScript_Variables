600c7b491c658abfde19b954a669becc429df0cb
jess <jcottrill1123@gmail.com>
2022-09-14 16:16:10 -0400

    completed exercises

--- app.js
+++ app.js
@@ -4,7 +4,40 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Jessica";
+let lastName = "Cottrill";
+var age = 33;
 
 // Exercise 2
+lastName = "Cottrill";
+age = 33;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was first released in ${createdYear}.`);
+
+//exercise 4
+let product = "V-Neck Shirt";
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(
+  `${product} in size ${selectedSize}, is $${price}, with ${stock} in stock.`
+);
+
+//exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`I was looking for a new book to read. I found ${title} written by ${author}. 
+I decided to start reading it. It has ${pageCount}, I have already read to page ${bookmark}.`);
